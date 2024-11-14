ec9dcd263dbb8f7b8bfd2e12f5a19c0f3ef880a8
Ali Arthur <ali@Mac.lan>
2024-11-14 08:53:10 -0500

    completed property aggregate used to represent a property in the system

---
+++ Wiseshare/src/Wiseshare.Domain/PropertyAggregate/Property.cs
@@ -0,0 +1,34 @@
+using System.Dynamic;
+using Wiseshare.Domain.Common.Models;
+using Wiseshare.Domain.PropertyAggregate.ValueObject;
+using Wiseshare.Domain.UserAggregate.ValueObjects;
+
+namespace Wiseshare.Domain.PropertyAggregate;
+
+public sealed class Property : AggregateRoot<PropertyId, Guid>{
+    public string Address {get; private set;}
+    public string Location {get; private set;}
+    public double PropertyValue {get; private set;}
+    public double SharePrice {get; private set;}
+    public int AvailableShares {get; private set;}
+
+    public DateTime CreatedDateTime {get; private set;}
+    public DateTime UpdatedDateTime {get; private set;}
+
+    private Property(string address, string location, double propertyValue,double sharePrice,
+                     int availableShares, PropertyId? propertyId = null)
+            : base(propertyId ?? PropertyId.CreateUnique())
+    {
+        Address = address;
+        Location = location;
+        PropertyValue = propertyValue;
+        SharePrice = sharePrice;
+        AvailableShares = availableShares;
+
+    }
+    public static Property Create(string address,string location,double propertyValue,double sharePrice, int availableShares)
+    {
+        return new Property(address,location,propertyValue,sharePrice,availableShares);
+
+    }
+}
---
+++ Wiseshare/src/Wiseshare.Domain/PropertyAggregate/ValueObjects/PropertyId.cs
@@ -0,0 +1,15 @@
+using Wiseshare.Domain.Common.Models;
+
+namespace Wiseshare.Domain.PropertyAggregate.ValueObject;
+
+public sealed class PropertyId : AggregateRootId<Guid> {
+    private PropertyId(Guid value) : base(value){}
+
+    public static PropertyId CreateUnique() {
+        return new PropertyId(Guid.NewGuid());
+    }
+
+    public static PropertyId Create(Guid propertyId){
+        return new PropertyId(propertyId);
+    }
+}
