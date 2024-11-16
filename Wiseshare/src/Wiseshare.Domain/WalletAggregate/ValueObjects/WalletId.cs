40e7eb294e53014542404b18167651be31a84b04
Ali Arthur <ali@Mac.lan>
2024-11-16 18:55:05 -0500

    completed wallet aggregate used to represent a user wallet in the system

---
+++ Wiseshare/src/Wiseshare.Domain/WalletAggregate/ValueObjects/WalletId.cs
@@ -0,0 +1,32 @@
+using Wiseshare.Domain.Common.Models;
+using Wiseshare.Domain.UserAggregate.ValueObjects;
+
+namespace Wiseshare.Domain.WalletAggregate.ValueObjects;
+
+public sealed class WalletId : AggregateRootId<string>
+{
+    // Private constructor to enforce the use of factory methods
+    private WalletId(string value) : base(value)
+    {
+    }
+
+    // Private constructor that generates WalletId based on UserId  If userId.Value is "User123", then $"Wallet_{userId.Value}" evaluates 
+    //to "Wallet_User123".This ensures that each WalletId is uniquely tied to a specific user, following a consistent naming convention.
+    private WalletId(UserId userId)
+        : base($"Wallet_{userId.Value}") 
+    {
+    }
+
+    // Factory method to create WalletId using UserId
+    public static WalletId Create(UserId userId)
+    {
+        // Validate that userId is not null or invalid if necessary
+        return new WalletId(userId);
+    }
+
+    // Factory method to create WalletId using a raw string value
+    public static WalletId Create(string value)
+    {
+        return new WalletId(value);
+    }
+}
---
+++ Wiseshare/src/Wiseshare.Domain/WalletAggregate/Wallet.cs
@@ -0,0 +1,30 @@
+using Wiseshare.Domain.Common.Models;
+using Wiseshare.Domain.WalletAggregate.ValueObjects;
+using Wiseshare.Domain.UserAggregate.ValueObjects;
+
+namespace Wiseshare.Domain.WalletAggregate;
+
+public sealed class Wallet : AggregateRoot<WalletId, string> {
+
+    public UserId UserId{get; private set;}
+    public decimal Balance {get; private set;}
+
+    public DateTime CreatedDateTime { get; private set; }
+    public DateTime UpdatedDateTime { get; private set; }
+
+
+
+private Wallet(UserId userId, decimal balance, DateTime createdDateTime, DateTime updatedDateTime)
+{
+    UserId = userId ?? throw new ArgumentNullException(nameof(userId)); //?? ensures userId is not null. if it is throw an eception
+    Balance = balance;
+    CreatedDateTime = createdDateTime;
+    UpdatedDateTime = updatedDateTime;
+}
+public static Wallet Create(UserId userId, decimal balance, DateTime createdDateTime, DateTime updatedDateTime)
+{
+    return new Wallet(userId, balance, createdDateTime, updatedDateTime);
+}
+
+}
+
